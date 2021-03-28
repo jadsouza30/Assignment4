dc6f09a71f66dbf454a0b8732f2662176bfe2ce4
jadsouza30 <jasondsouza0530@gmail.com>
2021-03-28 02:42:26 -0400

    util

---
+++ treact/src/backend/util.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+
+export const getUser= ()=>{
+    return new Promise((resolve,reject)=>
+    {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          resolve(user)
+        } else {
+          resolve(null)
+        }
+      });
+    })
+}
